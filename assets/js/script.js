2419eb596c3f0e44844f4e11a8a2d66e31a36f32
Patrick <lipatrick093@gmail.com>
2023-11-08 20:46:04 -0500

    test push

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function fetchRecipeDetails() {
+    const apiKey = 'ef88ad079b864a8ebb40054e93b35c0d'; // Replace with your API key
+    const recipeId = 'RECIPE_ID'; // Replace with the recipe ID you want to fetch
+    const searchRecipe = 'https://api.spoonacular.com/recipes/complexSearch';
+    const getRecipe = 'https://api.spoonacular.com/recipes/{id}/information';
+
+fetch(searchRecipe)
+//when data is fetched
+// then find response
+// create variable for title and ingredients list
+// display data
+// append to html
+
+}
+
